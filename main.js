e052a5eb0f049920d493f6c1def6568d30972769
marcos torres <marcosezequiel_torres@hotmail.com>
2022-04-09 13:23:09 -0300

    insertado el scroll reveal

--- main.js
+++ main.js
@@ -6,4 +6,10 @@ boton.addEventListener("click",()=>{
     document.querySelector(".nav-menu").classList.toggle("show");
 })
 
+ScrollReveal().reveal(".showcase");
+ScrollReveal().reveal(".news-cards",{delay:500});
+ScrollReveal().reveal(".cards-banner-one",{delay:500});
+ScrollReveal().reveal(".cards-banner-two",{delay:500});
+ScrollReveal().reveal(".social",{delay:500});
+ScrollReveal().reveal(".footer-links",{delay:500});
 
